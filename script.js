c9eb8be72831558eb999f218a4375b1ee316067f
jenanF <108689302+jenanF@users.noreply.github.com>
2022-10-30 05:59:36 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 
+function BMI(weight = 70, height = 1.8){
+
+    let result = weight / (height * height);
+    return result;
+}
+console.log(BMI(61, 163))
+console.log(BMI())
+
+function Status(BodyMass){
+
+    let res = document.getElementById("result")
+
+    if(BodyMass < 18.5){
+        res.style.color="orange"
+        return"you are underweight"
+       
+    }
+    else if(BodyMass >= 18.5 && BodyMass < 25){
+        res.style.color="green"
+        return "you weight is healthy"
+        
+    }
+    else if(BodyMass >= 25){
+        res.style.color="red"
+        return "you are overweight"
+        
+    }
+}
+
+function calculate(){
+
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi_value = BMI(weight, height);
+    let desc = Status(bmi_value);
+    let box = document.getElementById('result');
+    box.innerText = bmi_value + " == " + desc;
+}
+
